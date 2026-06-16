e83c4770c6c47e003b86482d1af29f720771bfbb
siteoficiall <gameflixoficial2.0@gmail.com>
2026-06-15 22:40:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const faqBtns = document.querySelectorAll('.faq-btn');
 
+faqBtns.forEach(btn => {
+
+btn.addEventListener('click', () => {
+
+const content = btn.nextElementSibling;
+
+if(content.style.display === 'block'){
+content.style.display = 'none';
+}else{
+content.style.display = 'block';
+}
+
+});
+
+});
